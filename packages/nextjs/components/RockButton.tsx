891fe1548f95c838a123c863aa0218a3330ab7ec
Aye Chan <ayechan.sant@gmail.com>
2023-07-20 12:21:30 -0700

    putting hook in RockButton

--- packages/nextjs/components/RockButton.tsx
+++ packages/nextjs/components/RockButton.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const RockButton = () => {
+  const { writeAsync } = useScaffoldContractWrite({
+    contractName: "YourContract",
+    functionName: "commit",
+    args: ["rock"],
+    onBlockConfirmation: txnReceipt => {
+      console.log("rock has been submitted", txnReceipt.blockHash);
+    },
+  });
+
   function handleClick() {}
 
   return (
