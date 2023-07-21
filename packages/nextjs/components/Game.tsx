0c218e647f502c0a6cbd963b14a5df5ce52b7f38
Aye Chan <ayechan.sant@gmail.com>
2023-07-21 16:32:49 -0700

    game component looks good and works

--- packages/nextjs/components/Game.tsx
+++ packages/nextjs/components/Game.tsx
@@ -7,15 +7,12 @@ export const Game = () => {
 
   function handleRock() {
     setChoice("rock");
-    writeAsync({ args: [choice, salt] });
   }
   function handlePaper() {
     setChoice("paper");
-    writeAsync({ args: [choice, salt] });
   }
   function handleScissors() {
     setChoice("scissors");
-    writeAsync({ args: [choice, salt] });
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -28,45 +25,49 @@ export const Game = () => {
     functionName: "commit",
     args: [choice, salt],
     onBlockConfirmation: txnReceipt => {
-      console.log("game created", txnReceipt.blockHash);
+      console.log("choice and salt written", txnReceipt.blockHash);
     },
   });
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div className="py-5">Salt your answer:</div>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={e => setSalt(e.target.value)}
-          type="string"
-          required
-        />
-        <div className="py-5">
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Submit
+      <div className="flex justify-center items-center flex-col py-4">
+        <div className="space-x-6">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={handleRock}
+          >
+            Rock
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={handlePaper}
+          >
+            Paper
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            onClick={handleScissors}
+          >
+            Scissors
           </button>
         </div>
-      </form>
-      <div className="flex space-x-6">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleRock}
-        >
-          Rock
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handlePaper}
-        >
-          Paper
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleScissors}
-        >
-          Scissors
-        </button>
+        {choice && <div className="py-3">You have chosen: {choice}</div>}
+
+        <form onSubmit={handleSubmit}>
+          <div className="py-5">Now salt your answer:</div>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={e => setSalt(e.target.value)}
+            type="string"
+            required
+          />
+          <div className="py-5">
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
