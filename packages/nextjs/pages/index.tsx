febba6aecc7854f0d511e67a530bb1c93ad97e8f
Aye Chan <ayechan.sant@gmail.com>
2023-07-25 11:58:17 -0700

    deleted example pages

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from "next";
 import { Welcome } from "~~/components/Welcome";
 
-//import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
-//import { MetaHeader } from "~~/components/MetaHeader";
-
 const Home: NextPage = () => {
   return (
     <>
